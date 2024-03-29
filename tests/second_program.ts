8ff73b5f0863cd14b5988690eb1943f4b8a4c0e9
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-03-30 04:26:48 +0530

    🔨Written tests for it

--- tests/second_program.ts
+++ tests/second_program.ts
@@ -1,16 +1,101 @@
+import assert from "assert";
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { SecondProgram } from "../target/types/second_program";
+import { SystemProgram } from "@solana/web3.js";
 
 describe("second_program", () => {
+  // Use a local provider.
+  const provider = anchor.AnchorProvider.local();
+
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.SecondProgram as Program<SecondProgram>;
+  let _myAccount: anchor.web3.Keypair;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Creates and initializes an account in a single atomic transaction (simplified)", async () => {
+    // The program to execute.
+    const program = anchor.workspace.Basic1;
+
+    // The Account to create.
+    const myAccount = anchor.web3.Keypair.generate();
+
+    // Create the new account and initialize it with the program.
+    await program.rpc.initialize({
+      accounts: {
+        myAccount: myAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [myAccount],
+    });
+
+    // Fetch the newly created account from the cluster.
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+    console.log(account)
+
+    // Check its state was initialized.
+    assert.equal(account.data, 0);
+
+    // Store the account for the next test.
+    _myAccount = myAccount;
+  });
+
+  it("Updates a previously created account", async () => {
+    const myAccount = _myAccount;
+
+    // The program to execute.
+    const program = anchor.workspace.Basic1;
+
+    // Invoke the update rpc.
+    await program.rpc.update(new anchor.BN(100), {
+      accounts: {
+        myAccount: myAccount.publicKey,
+      },
+    });
+
+    // Fetch the newly updated account.
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+
+    // Check its state was mutated.
+    assert.equal(account.data, 100);
+  });
+
+  it("Increment", async () => {
+    const myAccount = _myAccount;
+
+    // The program to execute.
+    const program = anchor.workspace.Basic1;
+
+    // Invoke the update rpc.
+    await program.rpc.increment({
+      accounts: {
+        myAccount: myAccount.publicKey,
+      },
+    });
+
+    // Fetch the newly updated account.
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+
+    // Check its state was mutated.
+    assert.equal(account.data, 101);
+  });
+
+  it("Decrement", async () => {
+    const myAccount = _myAccount;
+
+    // The program to execute.
+    const program = anchor.workspace.Basic1;
+
+    // Invoke the update rpc.
+    await program.rpc.decrement({
+      accounts: {
+        myAccount: myAccount.publicKey,
+      },
+    });
+
+    // Fetch the newly updated account.
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+
+    // Check its state was mutated.
+    assert.equal(account.data, 100);
   });
 });
